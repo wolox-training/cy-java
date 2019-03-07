7cd83423ae3c5bd14bb6e97054401c9307f8c7bd
Cristian Yanza <cristian.yanza@wolox.co>
2019-03-07 12:29:31 -0500

    user-crud, fix merge

--- src/main/java/wolox/training/exceptions/BookAlreadyOwnedException.java
+++ src/main/java/wolox/training/exceptions/BookAlreadyOwnedException.java
@@ -3,7 +3,7 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Book Already Owned")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Book Already owned")
 public class BookAlreadyOwnedException extends RuntimeException {
 
     public BookAlreadyOwnedException(String message) {
