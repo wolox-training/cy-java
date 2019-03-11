f7965af4d5df2626623659db2526f506911c2e55
Cristian Yanza <cristian.yanza@wolox.co>
2019-03-11 14:37:22 -0500

    Api-externa, deleted comment

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -27,9 +27,6 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-//    @Autowired
-//    private OpenLibraryService openLibraryService;
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
